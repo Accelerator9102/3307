408b44f493c107a72966c2c993c169cc8b2b2019
Xiaoyu <xxie66@uwo.ca>
2019-11-27 16:13:11 -0500

    3307

--- mywidget.cpp
+++ mywidget.cpp
@@ -9,13 +9,29 @@ mywidget::mywidget(QWidget *parent)
     QPushButton *btn = new QPushButton;
     btn->setParent(this);
     btn->setText("Start!");
+    btn->setStyleSheet("background-color: rgb(0,255,127);"
+                       "border-style:solid;"
+                       "border-width:5px;"
+                       "border-color: #339;"
+                       "font-size: 30px;");
+
     QPushButton *btn2 = new QPushButton("Close!",this);
-    btn->move(100,100);
-    btn2->move(300,100);
-    btn2->resize(50,50);
+    btn2->setStyleSheet("background-color: rgb(0,255,255);"
+                        "border-style:solid;"
+                        "border-width: 5px;"
+                        "border-color: #339;"
+                        "font-size: 30px;");
+    btn->move(600,700);
+    btn2->move(1240,700);
+    btn->resize(200,100);
+    btn2->resize(200,100);
+
+
     resize(600,400);
-    setFixedSize(600,400);
+    setFixedSize(1980,1020);
     setWindowTitle("Welcome!");
+    this->setStyleSheet("background-color:pink;");
+
 
     connect(btn2,&QPushButton::clicked,this,&mywidget::close);
 }
